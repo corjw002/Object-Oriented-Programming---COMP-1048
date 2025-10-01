a2d32107dd1e74f57ef22596270395c835fc7122
JosephCornwell <cornjw002@mymail.unisa.edu.au>
2025-10-01 16:40:12 +0930

    Cleaned the code to make it user-friendly.  Added comments.

--- Activity 2.1 - Debugging Case Study #1.py
+++ Activity 2.1 - Debugging Case Study #1.py
@@ -2,14 +2,16 @@ class Cat:
     # Made double underscores and changed spelling from bread to breed.
     def __init__(self, name, age, breed):
         self.name = name
-        self.age = 1
-        self.breed = "Devon Rex"
+
+        # Refer these attributes to parameters to make the code more practical.
+        self.age = age
+        self.breed = breed
 
     # Included "self" in the bracket here.
     def walk(self):
         print("*Evasiveley strolling*")
 
 
-# Included age and breed here.
-Cat = Cat("Kora", 2, "Ragdoll")
+# Included age and breed here instead of in the class construction.
+Cat = Cat("Kora", 1, "Devon Rex")
 Cat.walk()
